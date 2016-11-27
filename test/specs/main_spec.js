7ca9acd8969ceeda24e3f6ce5a89307da5ceed48
trentonlawton <trentonlawton@gmail.com>
2016-11-27 12:07:20 -0800

    Modified karma conf to update karma html reporter and added more test to both the render and save functionality

--- test/specs/main_spec.js
+++ test/specs/main_spec.js
@@ -1,14 +1,60 @@
-describe("Objective",function(){
+describe("Objective", function() {
 
-	describe("Render",function(){
 
-	it("Should Render ui",function(){
+	/*
 
-		expect("foo").toEqual("cow");
+Render logic
+	*/
+
+  describe("Render", function() {
+
+    it("Should Render setup ui if the user doesnt have stored data",
+       function() { expect("foo").toEqual("cow"); })
+
+	  it("should Render the current daily objectives if the user has daily stored objectives",
+       function() { expect("food").toEqual("cow") })
+
+	  it("should Render previous daily Objectives if the user has previously completed daily objectives",
+       function() {
+         expect("foo").toEqual("cow")
+       })
+
+		it("should render the current time left to complete daily objectives if they exist",
+			function(){
+				expect("foo").toEqual("cow")
+		})
+
+		it("should render a help view when the user clicks a help tooltip",
+			function(){
+				expect("foo").toEqual("cow")
+
+		})
+
+  })
+/*
+Saving logic
+*/
+
+describe("Save",function(){
+
+	it("should save setup info when the user clicks done to local storage",function(){
+		expect("foo").toEqual('cow')
+
+	})
+	it("should save completed state to true on objective object if a user double clicks a rendered objective",
+function(){
+expect("foo").toEqual("cow")
+
+})
+it("should save completed objective to past objectives if the user double clicks a daily objective",
+function(){
+epect("foo").toEqual("cow")
+
 })
 
 
-})
 
+
+})
 
 })
