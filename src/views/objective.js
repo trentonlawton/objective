0f58fd5166f74d5eb04761a68535fe8961543348
trentonlawton <trentonlawton@gmail.com>
2016-11-28 18:16:05 -0800

    Ui linked styles to objective component and created file for difficulty color array

--- src/views/objective.js
+++ src/views/objective.js
@@ -1,26 +1,26 @@
 import React, {Component} from 'react';
+import "../styles/objective.css"
 import $ from 'jquery';
 // import '../styles/dailyForm.css';
 
 class objective extends Component {
   constructor(props) {
     super(props);
-    this.state={ data : 'test', difficulty : 1, completed : 'false' };
+    this.state={ data : 'Take the trash out', difficulty : 1, completed : 'false' };
 
-    setInterval(() => {
-     this.setState({difficulty: this.state.difficulty + 2})
-   },1000)
-this.handleclick = this.handleclick.bind(this);
+//     setInterval(() => {
+//      this.setState({difficulty: this.state.difficulty + 2})
+//    },1000)
+// this.handleclick = this.handleclick.bind(this);
 
   }
-  handleclick(){
-this.setState({data:"gotcha"})}
+//   handleclick(){
+// this.setState({data:"gotcha"})}
   render() {
     return (
-      <div className="mainView">
-<h2 onClick={this.handleclick}>{this.state.data}</h2>
-<h2>{this.state.difficulty}</h2>
-<h2>{this.state.completed}</h2>
+      <div className="objectiveContainer">
+<p><span>{this.state.difficulty}</span>"{this.state.data}"</p>
+
       </div>
     );
   }
